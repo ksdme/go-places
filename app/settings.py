b0097403c6a24450732388fea447746b766394ea
Kilari Teja <kilariteja9@gmail.com>
2023-06-05 18:15:10 +0530

    Configure production values

--- app/settings.py
+++ app/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import ast
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "r!8_-p8byuw7eb$e7&gcr#05poastj%2mtn$1l&xussd^&u$6_"
+SECRET_KEY = os.environ.get("SECRET", "r!8_-p8byuw7eb$e7&gcr#05poastj%2mtn$1l&xussd^&u$6_")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = ast.literal_eval(os.environ.get("DEBUG", "True"))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [] if DEBUG else [os.environ.get("SITE_HOST")]
 
 
 # Application definition
@@ -81,7 +83,7 @@ WSGI_APPLICATION = "app.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": os.environ.get("DATABASE", BASE_DIR / "db.sqlite3"),
     }
 }
 
