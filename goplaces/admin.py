3b31be04b98f6697112b8cf44397c88de8a333fc
Kilari Teja <kilariteja9@gmail.com>
2023-06-05 17:18:21 +0530

    Fix django admin for auth models

--- goplaces/admin.py
+++ goplaces/admin.py
@@ -19,3 +19,15 @@ class LinkModelAdmin(ModelAdmin):
 
     def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
         return super().get_queryset(request).filter(owner=request.user)
+
+
+import django.contrib.auth.models as auth_models
+import django.contrib.auth.admin as auth_admin
+
+admin.site.unregister(auth_models.User)
+@admin.register(auth_models.User)
+class UserAdmin(auth_admin.UserAdmin, ModelAdmin): pass
+
+admin.site.unregister(auth_models.Group)
+@admin.register(auth_models.Group)
+class GroupAdmin(auth_admin.GroupAdmin, ModelAdmin): pass
--- goplaces/apps.py
+++ goplaces/apps.py
@@ -5,3 +5,10 @@ class GoPlacesConfig(AppConfig):
     name = "goplaces"
     verbose_name = "App"
     default_auto_field = "django.db.models.BigAutoField"
+
+    def ready(self) -> None:
+        # Hack to fix the name of the auth app in the django-admin.
+        from django.contrib.auth.apps import AuthConfig
+        AuthConfig.verbose_name = "Users"
+
+        return super().ready()
